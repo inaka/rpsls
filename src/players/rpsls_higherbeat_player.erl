50d27ef050c6b08f8daa043cb7d10f445ec738e2
Elian Gidoni <elian@braceloop.com>
2014-07-26 23:18:16 -0300

    Clean up.

--- src/players/rpsls_higherbeat_player.erl
+++ src/players/rpsls_higherbeat_player.erl
@@ -16,27 +16,14 @@ play([{Last, Mine},{Mine, _}|_], State) -> {beat_copycat(Last), State};
 play([{_, Repeat},{_, Repeat }, {_, Repeat}|_], State) -> {beat_copycat(Repeat), State};
 play([{Last,_}|_], State) -> {beat_winnersof(Last), State}.
 
-%beat_winnersof(rock) -> lizard;
 beat_winnersof(rock) -> lists:nth(random:uniform(2), [lizard, scissors]);
-
-%beat_winnersof(paper) -> rock;
 beat_winnersof(paper) -> lists:nth(random:uniform(2), [rock, spock]);
-
-%beat_winnersof(scissors) -> paper;
 beat_winnersof(scissors) -> lists:nth(random:uniform(2), [paper, lizard]);
-
-%beat_winnersof(lizard) -> spock;
 beat_winnersof(lizard) -> lists:nth(random:uniform(2), [spock, paper]);
-
-%beat_winnersof(spock) -> scissors;
-beat_winnersof(spock) -> lists:nth(random:uniform(2), [scissors, rock]);
-
-beat_winnersof(_) -> scissors. %% cannot happen but give scissors as default
+beat_winnersof(spock) -> lists:nth(random:uniform(2), [scissors, rock]).
 
 beat_copycat(rock) -> lists:nth(random:uniform(2), [paper, spock]);
 beat_copycat(paper) -> lists:nth(random:uniform(2), [scissors, lizard]);
 beat_copycat(scissors) -> lists:nth(random:uniform(2), [rock, spock]);
 beat_copycat(lizard) -> lists:nth(random:uniform(2), [scissors, rock]);
-beat_copycat(spock) -> lists:nth(random:uniform(2), [paper, lizard]);
-beat_copycat(_) -> scissors.
-
+beat_copycat(spock) -> lists:nth(random:uniform(2), [paper, lizard]).
