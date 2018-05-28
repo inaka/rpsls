6994a3d39cae3acab8b150ddeca099d47436a8d1
Brujo Benavides <elbrujohalcon@inaka.net>
2018-05-27 22:34:28 -0300

    Get the project lint-ed by elvis

--- test/rpsls_bart_player_SUITE.erl
+++ test/rpsls_bart_player_SUITE.erl
@@ -1,17 +1,9 @@
 -module (rpsls_bart_player_SUITE).
--export([all/0, test/1, test2/1, test3/1, test4/1]).
 
-all() ->
-  [test, test2, test3, test4].
+-export([all/0, test/1]).
 
-test(_) -> {draw, _, [{rock,rock}]} = rpsls:play(rpsls_bart_player,rpsls_bart_player,1).
+all() -> [test].
 
-test2(_) -> {draw, _, [{rock,rock}, {rock,rock}]} = rpsls:play(rpsls_bart_player,rpsls_bart_player,2).
-
-test3(_) -> {draw, _, [{rock,rock}, {rock,rock}, {rock,rock}]} = rpsls:play(rpsls_bart_player,rpsls_bart_player,3).
-
-test4(_) -> {draw, _, [{rock,rock}, {rock,rock}, {rock,rock}, {rock,rock}]} = rpsls:play(rpsls_bart_player,rpsls_bart_player,4).
-
-
-
-
+test(_) ->
+  {draw, _, History} = rpsls:play(rpsls_bart_player, rpsls_bart_player, rand:uniform(1000) + 1),
+  [{rock, rock}] = lists:usort(History).
